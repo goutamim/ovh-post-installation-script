6e8213f110931e00b712d970faf792c06bb7681a
goutamim <43399110+goutamim@users.noreply.github.com>
2018-09-26 12:59:44 +0530

    Update partition.sh

--- partition.sh
+++ partition.sh
@@ -10,22 +10,23 @@ sed -i -E  "s/^#?Port 22/Port 2022/g" /etc/ssh/sshd_config
 sed -i -E  "s/^#?UseDNS no/UseDNS no/g" /etc/ssh/sshd_config
 
 #disk partition creation
-disks=$(lshw -class disk -short|grep disk |awk '{print $2}')
-count=0  							#count variable to make a partition on sda diffrently
+disks=$(lsblk -l | grep disk | awk '{print $1}')
+
 for i in $disks
 do
-    count=$[$count +1]
-    echo $i
-    if [ $count -eq 1 ]
+   
+    echo "creating partion on disk /dev/$i"
+    fdisk -l /dev/$i |grep "Device"
+    if [[ $? -eq 0 ]];
       then
 	      #start_freespace to get the start of the free space 
-	      start_freespace=$(parted "$i" unit s print free |grep Free |tail -1 | awk '{print $1}')
+	      start_freespace=$(parted "/dev/$i" unit s print free |grep Free |tail -1 | awk '{print $1}')
 	      start=${start_freespace::-1}
 	      #echo $start
 	      #creating partition on  disk
-	      (echo n; echo p; echo "$start"; echo " ";echo t;echo "";echo 8e;echo w) |fdisk $i
+	      (echo n; echo p; echo "$start"; echo " ";echo t;echo "";echo 8e;echo w) |fdisk /dev/$i
       else
-	      (echo n; echo p; echo ""; echo "";echo "";echo yes;echo t;echo 8e;echo w) |fdisk $i
+	      (echo n; echo p; echo ""; echo "";echo "";echo t;echo 8e;echo w) |fdisk /dev/$i
     fi
 done
 
@@ -43,11 +44,14 @@ lvcreate --name data -l 100%FREE vg_data
 mkfs.ext4  /dev/vg_data/data -F
 
 #appending a line to a /etc/fstab file ,if it doesnt exist
-grep vg_data /etc/fstab
+grep vg_data /etc/fstab    
 if [[ $? -ne 0 ]];
 then
 echo "/dev/vg_data/data       /data   ext4    errors=remount-ro,discard       0       1" >> /etc/fstab
 fi
+
+#creating directory 
+mkdir -p /data
 
 #mounting
 mount -a
